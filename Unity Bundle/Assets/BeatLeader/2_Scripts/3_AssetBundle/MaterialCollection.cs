00c3d23382cfe93b01df9665219b6c79ccc4cd83
hermanest <nicedevelooper@gmail.com>
2024-10-29 12:09:37 +0300

    Added tinted blur material version.

--- Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/MaterialCollection.cs
+++ Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/MaterialCollection.cs
@@ -4,12 +4,9 @@ namespace BeatLeader {
     [CreateAssetMenu(fileName = "MaterialCollection", menuName = "MaterialCollection")]
     public class MaterialCollection : ScriptableObject {
         public Material blurMaterial;
+        public Material tintedBlurredBackgroundMaterial;
         public Material blurredBackgroundMaterial;
-        public Material plotterMaterial;
-        public Material applicatorMaterial;
         public Material uiAdditiveGlowMaterial;
         public Material uiNoDepthMaterial;
-        public Material uiRoundTexture2Material;
-        public Material uiRoundTexture10Material;
     }
 }
