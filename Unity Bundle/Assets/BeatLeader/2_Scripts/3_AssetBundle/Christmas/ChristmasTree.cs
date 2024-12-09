4d9238348aee5c7ff213a47057d5bad8773c1d6d
Hermanest <nicedevelooper@gmail.com>
2024-12-10 01:27:44 +0300

    Added tree icon and animator.

--- Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/Christmas/ChristmasTree.cs
+++ Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/Christmas/ChristmasTree.cs
@@ -3,42 +3,12 @@ using UnityEngine;
 
 namespace BeatLeader {
     public class ChristmasTree : MonoBehaviour {
-        [SerializeField]
-        public ChristmasTreeLevel[] _levels;
-
-        [SerializeField]
-        private Transform _animationContainer;
-
+        [SerializeField] private ChristmasTreeLevel[] _levels;
+        [SerializeField] private CapsuleCollider _collider;
+        [SerializeField] private ChristmasTreeAnimator _animator;
         public bool gizmos;
-        public float animationSpeed = 10f;
 
-        private float _targetScale;
-        private bool _set = true;
-
-        public void Present() {
-            _targetScale = 1f;
-            _set = false;
-        }
-
-        public void Dismiss() {
-            _targetScale = 0f;
-            _set = false;
-        }
-
-        private void Update() {
-            if (_set) return;
-
-            var t = Time.deltaTime * animationSpeed;
-            var targetScale = _targetScale * Vector3.one;
-            var scale = Vector3.Lerp(_animationContainer.localScale, targetScale, t);
-
-            if (Mathf.Abs(scale.x - targetScale.x) <= 0.001f) {
-                scale = targetScale;
-                _set = true;
-            }
-
-            _animationContainer.localScale = scale;
-        }
+        #region Editor
 
         public Vector3 Align(Vector3 pos) {
             var y = pos.y;
@@ -64,5 +34,7 @@ namespace BeatLeader {
                 t.Draw(transform.lossyScale.x);
             }
         }
+
+        #endregion
     }
 }
---
+++ Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/Christmas/ChristmasTreeAnimator.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace BeatLeader {
+    public class ChristmasTreeAnimator : MonoBehaviour {
+        [SerializeField] private float _animationSpeed = 10f;
+    }
+}
