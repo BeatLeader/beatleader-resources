cf6802bff5c0eed63f173881a3d0dc864ff75579
Hermanest <nicedevelooper@gmail.com>
2024-12-17 16:38:07 +0300

    Added snow.

---
+++ Unity Bundle/Assets/BeatLeader/2_Scripts/3_AssetBundle/Christmas/SnowController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace BeatLeader {
+    public class SnowController : MonoBehaviour {
+        [SerializeField]
+        private ParticleSystem _particleSystem;
+
+        private void Start() {
+            Play(true);
+        }
+
+        public void Play(bool immediate) {
+            var emission = _particleSystem.emission;
+            emission.enabled = true;
+            if (immediate) {
+                var duration = _particleSystem.main.startLifetimeMultiplier;
+                _particleSystem.Simulate(duration, true, true, true);
+            }
+            _particleSystem.Play();
+        }
+
+        public void Stop(bool immediate) {
+            var emission = _particleSystem.emission;
+            emission.enabled = false;
+        }
+    }
+}
